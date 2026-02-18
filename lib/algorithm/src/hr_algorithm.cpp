3d7d06cf3bd140321a618059c4b1467bfeeed1b2
jiechuan1011 <978076227@qq.com>
2026-02-18 11:52:28 +0800

    Auto-commit: hr_algorithm.cpp - 2026-02-18 11:52:28

---
+++ lib/algorithm/src/hr_algorithm.cpp
@@ -0,0 +1,6 @@
+// Copied from algorithm/hr_algorithm.cpp to lib for LDF
+#include "hr_algorithm.h"
+#include "motion_correction.h"
+
+// (implementation copied from original algorithm/hr_algorithm.cpp)
+#include "../../algorithm/hr_algorithm.cpp"
