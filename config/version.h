16194871a09548e93defaa05b06f375d45be9a3b
jiechuan1011 <978076227@qq.com>
2026-02-18 11:16:10 +0800

    Auto-commit: 1个文件变化 - 2026-02-18 11:16:09

--- config/version.h
+++ config/version.h
@@ -1,9 +1,9 @@
-(#ifndef VERSION_H)
+#ifndef VERSION_H
 #define VERSION_H
 
 // 项目版本信息
 #define SOFTWARE_VERSION_STRING "v0.0.0"
-#define BUILD_DATE_STRING __DATE__ " " __TIME__
+#define BUILD_DATE_STRING (__DATE__ " " __TIME__)
 
 #endif // VERSION_H
 
